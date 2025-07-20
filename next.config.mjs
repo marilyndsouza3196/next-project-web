adab2ba62e2420c2f69f56b43ffb3fdbd1e091ba
Marilyn <marilyndsouza3196@gmail.com>
2025-07-20 23:31:43 +0530

    rediect commit to 18squares

--- next.config.mjs
+++ next.config.mjs
@@ -15,19 +15,34 @@
 
 // export default nextConfig;
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     async redirects() {
+//       return [
+//         {
+//           source: '/:path*',
+//           has: [
+//             {
+//               type: 'host',
+//               key: 'host',
+//               value: 'www.18squares.com',
+//             },
+//           ],
+//           destination: 'https://18squares.com/:path*',
+//           permanent: true,
+//         },
+//       ];
+//     },
+//   };
+  
+//   module.exports = nextConfig;
+  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async redirects() {
       return [
         {
           source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              key: 'host',
-              value: 'www.18squares.com',
-            },
-          ],
           destination: 'https://18squares.com/:path*',
           permanent: true,
         },
