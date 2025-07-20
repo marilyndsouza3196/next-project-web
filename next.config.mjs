88de22791f58dfb652c41d7172b28fa81745dfd9
Marilyn <marilyndsouza3196@gmail.com>
2025-07-20 23:28:35 +0530

    ix redirect from www to 18squares.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,39 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     async redirects() {
+//         return [
+//           {
+//             source: '/:path*',
+//             has: [{ type: 'host', value: 'www.18squares.com' }],
+//             destination: 'https://18squares.com/:path*',
+//             permanent: true,
+//           },
+//         ];
+//       },
 
-export default nextConfig;
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              key: 'host',
+              value: 'www.18squares.com',
+            },
+          ],
+          destination: 'https://18squares.com/:path*',
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
